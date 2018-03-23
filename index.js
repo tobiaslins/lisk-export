778e0ed2806bfb7ff1c8af5cbf65a27ab81f9696
Tobias Lins <tobias.lins@mpay24.com>
2018-03-23 10:00:36 +0100

    format amounts as in lisk hub

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const lisk = require('lisk-js')
 const json2csv = require('json2csv').parse
 const { send } = require('micro')
 const { router, get } = require('microrouter')
+const BigNumber = require('bignumber.js')
+
+const fromRawLsk = value => (
+  new BigNumber(value || 0).dividedBy(new BigNumber(10).pow(8)).toFixed()
+)
 
 module.exports = router(
   get('/:address', async (req, res) => {
@@ -16,8 +21,19 @@ module.exports = router(
           'Content-Disposition',
           `attachment; filename=lisk_transactions_${address}.csv`
         )
+        const transactions = data.transactions.map(({id, height, blockId, timestamp, senderId, recipientId, amount, fee, confirmations}) => ({
+          id,
+          height,
+          blockId,
+          timestamp,
+          senderId,
+          recipientId,
+          amount: fromRawLsk(amount),
+          fee: fromRawLsk(fee),
+          confirmations
+        }))
         try {
-          const csv = json2csv(data.transactions, {
+          const csv = json2csv(transactions, {
             delimiter
           })
           resolve(csv)
